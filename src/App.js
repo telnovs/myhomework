55550ba3b9df594654621d51f79bc83b30c10a62
telnovs <telnov-s@mail.ru>
2020-03-15 00:07:47 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="wrapper">
+                <div className="cloud">
+                    <div className="name">
+                        Тельнов Сергей
+                    </div>
+                    <div className="message">
+                        Привет "МИР"!!!!!!!!
+                        Учу я реакт !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    </div>
+                    <div className="time">
+                        09:05 AM
+                    </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default App;
