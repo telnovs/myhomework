e24162ee7a535e63b7565ddb5dbfb522f73939f2
telnovs <telnov-s@mail.ru>
2020-03-15 01:41:35 +0700

    components could

---
+++ src/companents/wrapper/Could/could.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+class  Could extends React.Component {
+    render = () => {
+        return (
+                <div className="cloud">
+                    <div className="name">
+                        Тельнов Сергей
+                    </div>
+                    <div className="message">
+                        Привет "МИР"!!!!!!!!
+                        Учу я реакт !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    </div>
+                    <div className="time">
+                        09:05 AM
+                    </div>
+                </div>
+
+        );
+    }
+}
+export default Could;
--- src/companents/wrapper/wrapper.jsx
+++ src/companents/wrapper/wrapper.jsx
@@ -1,21 +1,11 @@
 import React from "react";
+import Could from "./Could/could";
 
 class Wrapper extends React.Component {
     render = () => {
         return (
 <div className="wrapper">
-    <div className="cloud">
-        <div className="name">
-            Тельнов Сергей
-        </div>
-        <div className="message">
-            Привет "МИР"!!!!!!!!
-            Учу я реакт !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        </div>
-        <div className="time">
-            09:05 AM
-        </div>
-    </div>
+    <Could/>
 </div>
         );
     }
